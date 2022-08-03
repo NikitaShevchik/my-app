45ff2372cf3ec7af4e6cef110d1d255345c0cd7e
NikitaShevchik <shevchik.nikita@gmail.com>
2022-08-03 16:00:14 +0300

    useRef, modules, css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+// import Counter from './components/counter';
+import ClassCounter from './components/ClassCounter';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './UI/button/MyButton';
+import MyInput from './UI/input/MyInput';
+import './styles/App.css';
 
 function App() {
-  const [likes, setLikes] = useState(5);
-  const [value, setValue] = useState('Текст в инпуте')
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript', body: 'JavaScript - язык программирования' },
+    { id: 2, title: 'Ruby', body: 'Ruby - для прожженных' },
+    { id: 3, title: 'Java', body: 'Java - 90 миллиардов устройств' },
+    { id: 4, title: 'HTML', body: 'Это наша жизнь' }
+  ])
 
-  function increment() {
-    setLikes(likes + 1)
-  }
-  function decrement() {
-    setLikes(likes - 1)
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Энергетик+', body: 'Наш ответ западу на его Джаву!' },
+    { id: 2, title: 'ТехноПрограЯз', body: 'ТехноПрограЯз - для опытных разработчиков' },
+    { id: 3, title: 'Верстак++', body: 'Верстак - 90 устройств' },
+    { id: 4, title: 'ПодклассникСтиль', body: 'Это наша красота' }
+  ])
+
+  const [title, setTitle] = useState('')
+  const bodyInputRef = useRef()
+
+  function addNewPost(e) {
+    e.preventDefault();
+    console.log(title);
+    console.log(bodyInputRef.current.value)
   }
 
   // function setInput() {
@@ -18,16 +38,23 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input
-        className='AppInput'
-        type="text"
-        value={value}
-        onChange={event => setValue(event.target.value)} // синхронизация (управляемые компоненты)
-      />
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <form>
+        {/* Управляемый компонент */}
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста"
+        />
+        {/* <MyInput
+          ref={bodyInputRef}
+          type="text"
+          placeholder="Описание поста"
+        /> */}
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title='Языки программирования (МИР)' />
+      <PostList posts={posts2} title='Языки программирования (СНГ)' />
     </div>
   );
 }
